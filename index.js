df4e09c4aaf1056a375f9e001d41482797fff1d3
AndreasHartov <trade4@hartov.dk>
2025-08-23 10:48:52 +0200

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-
-export default function Home() {
-  return (
-    <div style={{ padding: "2rem", fontFamily: "Arial, sans-serif" }}>
-      <h1>Welcome to NordLead Console</h1>
-      <p>This is the beginning of your new Next.js application.</p>
-    </div>
-  );
-}
